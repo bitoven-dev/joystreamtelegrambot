f4f971f91855d1e7117b1a72fc0de4035e9cf8ac
bitoven <62682550+bitoven-dev@users.noreply.github.com>
2020-07-13 11:33:19 +0700

    handling null

--- council.js
+++ council.js
@@ -19,42 +19,49 @@ async function main () {
     const unsubscribe = await api.rpc.chain.subscribeNewHeads(async (header) => {
         const block = header.number.toNumber()
         const councilround = await api.query.councilElection.round()
+        const councilendterm = await api.query.council.termEndsAt()
         const annperiod = await (await api.query.councilElection.announcingPeriod()).toNumber()
         const votingperiod = await (await api.query.councilElection.votingPeriod()).toNumber()
         const revealingperiod = await (await api.query.councilElection.revealingPeriod()).toNumber()
         const councilstage = await getcouncilStage(api)
         const councilperiod = await api.query.councilElection.newTermDuration()
-        const annstage = councilstage.Announcing
-        const votingstage = councilstage.Voting
-        const revealingstage = councilstage.Revealing
-        if (annstage>0) {
-            console.log('Announcing Stage')
-            if (block>lastcouncilnotif){
-                bot.sendMessage(chatid, `New council election for round ${councilround} has been started at block ${annstage-annperiod}. You can apply now!`, { parse_mode: 'html' })
-                lastcouncilnotif=annstage
-            }
-            
-        }
-        if (votingstage>0) {
-            console.log('Voting Stage')
-            if (block>lastcouncilnotif){
-                bot.sendMessage(chatid, `Voting stage for council election has been started at block ${votingstage-votingperiod}. You can vote now!`, { parse_mode: 'html' })
-                lastcouncilnotif=votingstage
-            }
-        }
-        if (revealingstage>0) {
-            console.log('Revealing Stage')
-            if (block>lastcouncilnotif){
-                bot.sendMessage(chatid, `Revealing stage for council election has been started at block ${revealingstage-revealingperiod}. Don't forget to reveal your vote!`, { parse_mode: 'html' })
-                lastcouncilnotif=revealingstage
-            }
-        }       
-        if (councilstage == null) {
-            console.log('Council has been elected')
-            if (block>lastcouncilnotif){
-                bot.sendMessage(chatid, `New council has been elected at block ${lastcouncilnotif}.`, { parse_mode: 'html' })
-                lastcouncilnotif=lastcouncilnotif+councilperiod
-            }
+
+        switch (councilstage){
+            case null:
+                console.log('Council has been elected')
+                if (block>lastcouncilnotif){
+                    bot.sendMessage(chatid, `New council has been elected at block ${councilendterm-councilperiod}.`, { parse_mode: 'html' })
+                    lastcouncilnotif=councilendterm
+                }
+                break;
+
+            default:
+                const annstage = councilstage.Announcing
+                const votingstage = councilstage.Voting
+                const revealingstage = councilstage.Revealing
+                if (annstage>0) {
+                    console.log('Announcing Stage')
+                    if (block>lastcouncilnotif){
+                        bot.sendMessage(chatid, `New council election for round ${councilround} has been started at block ${annstage-annperiod}. You can apply now!`, { parse_mode: 'html' })
+                        lastcouncilnotif=annstage
+                    }
+                }
+                if (votingstage>0) {
+                    console.log('Voting Stage')
+                    if (block>lastcouncilnotif){
+                        bot.sendMessage(chatid, `Voting stage for council election has been started at block ${votingstage-votingperiod}. You can vote now!`, { parse_mode: 'html' })
+                        lastcouncilnotif=votingstage
+                    }
+                }
+                if (revealingstage>0) {
+                    console.log('Revealing Stage')
+                    if (block>lastcouncilnotif){
+                        bot.sendMessage(chatid, `Revealing stage for council election has been started at block ${revealingstage-revealingperiod}. Don't forget to reveal your vote!`, { parse_mode: 'html' })
+                        lastcouncilnotif=revealingstage
+                    }
+                }       
+
+                break;
         }
     })
 }
